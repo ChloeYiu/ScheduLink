14eb62e4cc5fa0d25775d85bd114a2a104cc4a15
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2023-11-18 14:15:44 +0000

    implement api

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import Image from 'next/image'
-import { Button } from '@/components'
+import { Button } from "@/components";
+
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
       <div>
-        <h3 className='font-bold text-5xl mb-2'>ScheduLink</h3>
+        <h3 className="font-bold text-5xl mb-2">ScheduLink</h3>
         <p></p>
       </div>
 
-      <div className='timeslots'>
+      <div className="timeslots">
         <Button text="Morning"/>
       </div>
-
     </main>
-  )
+  );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+"use client";
+import React from "react";
+import axios from "axios";
+
+const uploadCal = async () => {
+  // fetch("/api/save-file", {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify("C:/Users/chloe_/Desktop/output.png"),
+  // });
+  axios
+    .get("/api/save-file", { params: { dir: "output.png" } })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => console.log(error));
+};
+
+const handleClick = () => {
+  uploadCal();
+};
 
 interface ButtonInputs {
-  text: String,
+  text: String;
 }
 
-const Button = ({text}: ButtonInputs) => {
+const Button = ({ text }: ButtonInputs) => {
   return (
-    <button className='w-100'>{text}</button>
-  )
-}
+    <button className="w-100" onClick={handleClick}>
+      {text}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
